664782f6d53308eb6caecab1d9bcce9d4cea3427
DavideTonin99 <davide9935@gmail.com>
2020-09-10 08:14:38 +0200

    add firebase connection, dao pattern

--- dao/DaoImpl.java
+++ dao/DaoImpl.java
@@ -1,31 +1,29 @@
 package elaborato_ingegneriaSW.dao;
 
+import com.google.cloud.firestore.Firestore;
+import elaborato_ingegneriaSW.utils.FirebaseConnection;
+
 import java.util.List;
 
 public abstract class DaoImpl<T> implements Dao<T> {
-    @Override
-    public List getAllItems() {
-        return null;
+    protected Firestore firestore;
+
+    protected DaoImpl() {
+        this.firestore = FirebaseConnection.getConnection();
     }
+
+    @Override
+    public abstract List getAllItems();
 
     @Override
     public abstract T getItem(String itemId);
 
     @Override
-    public abstract boolean addItem(Object item);
+    public abstract boolean addItem(T item);
 
     @Override
-    public boolean updateItem(Object item) {
-        return false;
-    }
+    public abstract boolean updateItem(T item);
 
     @Override
-    public boolean deleteItem(Object item) {
-        return false;
-    }
-
-    @Override
-    public boolean updateSource() {
-        return false;
-    }
+    public abstract boolean deleteItem(T item);
 }
