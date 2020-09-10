2c10f47da91954b2651659784a40fa144c53e387
DavideTonin99 <davide9935@gmail.com>
2020-09-10 23:29:12 +0200

    grafica

--- controllers/MainController.java
+++ controllers/MainController.java
@@ -41,7 +41,7 @@ public class MainController implements Initializable, ColorChangeCallback {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/elaborato_ingegneriaSW/views/SidePanelRicercatoreAnalista.fxml"));
             VBox box = loader.load();
-            SidePanelRicercatoreAnalistaController controller = loader.getController();
+            // SidePanelController controller = loader.getController();
             drawer.setSidePane(box);
         } catch (IOException ex) {
             Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
