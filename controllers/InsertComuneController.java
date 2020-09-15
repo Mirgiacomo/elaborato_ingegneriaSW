e8546fd80745f2472e417f23008e94bd19c032d6
mirgiacomo <mirgiacomo@gmail.com>
2020-09-15 18:52:02 +0200

    Refactoring font, layout, menu e id items view Comune

--- controllers/InsertComuneController.java
+++ controllers/InsertComuneController.java
@@ -1,12 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package elaborato_ingegneriaSW.controllers;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
+
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import elaborato_ingegneriaSW.dao.RegioneDaoImpl;
+import elaborato_ingegneriaSW.models.Provincia;
+import elaborato_ingegneriaSW.models.Regione;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -15,6 +22,22 @@ import javafx.fxml.Initializable;
  * @author mirgi
  */
 public class InsertComuneController implements Initializable {
+    @FXML
+    private JFXTextField ISTATTextField;
+    @FXML
+    private JFXDatePicker dataIstituzioneDataPicker;
+    @FXML
+    private JFXTextField nomeComuneTextField;
+    @FXML
+    private JFXTextField superficieTextField;
+    @FXML
+    private JFXComboBox tipoTerritorioComboBox;
+    @FXML
+    private JFXCheckBox fronteMareCheckBox;
+    @FXML
+    private JFXComboBox ProvinciaCollegataComboBox;
+
+
 
     /**
      * Initializes the controller class.
@@ -22,6 +45,11 @@ public class InsertComuneController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    @FXML
+    private void insertComuneAction(ActionEvent event) throws ExecutionException, InterruptedException {
+        System.out.println(ISTATTextField.getText());
+    }
     
 }
