4755af3372b5b9c155b55df36b8a2cbe5303b867
DavideTonin99 <davide9935@gmail.com>
2020-09-15 11:15:05 +0200

    fix

--- models/Territorio.java
+++ models/Territorio.java
@@ -1,7 +1,7 @@
 package elaborato_ingegneriaSW.models;
 
 public enum Territorio {
-    MONTANO,
-    COLLINARE,
-    PIANGEGGIANTE,
+    MONTAGNA,
+    COLLINA,
+    PIANURA,
 }
--- utils/AlertUtil.java
+++ utils/AlertUtil.java
@@ -1,12 +1,15 @@
 package elaborato_ingegneriaSW.utils;
 
-import elaborato_ingegneriaSW.MainApp;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
+
+import javafx.event.ActionEvent;
 
 public class AlertUtil {
 
-    public static void Alert(Alert.AlertType alertType, String title, String headerText, String contentText) {
+    public static void Alert(Alert.AlertType alertType, String title, String headerText, String contentText, ActionEvent event) {
         Alert alert = new Alert(alertType);
+        alert.initOwner(((Node)(event.getSource())).getScene().getWindow());
         alert.setTitle(title);
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
