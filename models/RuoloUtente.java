d9e0b2609020c48fd7aa77a92b46918c531f1030
DavideTonin99 <davide9935@gmail.com>
2020-09-18 09:49:08 +0200

    refactor ruolo utente

--- models/RuoloUtente.java
+++ models/RuoloUtente.java
@@ -3,6 +3,7 @@ package elaborato_ingegneriaSW.models;
 public enum RuoloUtente {
     ADMIN,
     PERSONALE_MONITORAGGIO,
-    PERSONALE_CONTRATTO,
+    PERSONALE_CONTAGI,
+    PERSONALE_DECESSI,
     RICERCATORE_ANALISTA,
 }
