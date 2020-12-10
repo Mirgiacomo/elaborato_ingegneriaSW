8d625d089caa1bba18c7e4409bfdd224555ea969
DavideTonin99 <davide9935@gmail.com>
2020-12-10 11:07:53 +0100

    refactoring view province

--- controllers/ViewProvinceController.java
+++ controllers/ViewProvinceController.java
@@ -9,11 +9,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -58,6 +60,10 @@ public class ViewProvinceController implements Initializable {
         Scene scene = new Scene(view);
 
         Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(
+                ((Node)event.getSource()).getScene().getWindow() );
+
         stage.setScene(scene);
         stage.showAndWait();
     }
