799a3bc37d091fc80f27947450f224d4c4d1eb60
mirgiacomo <mirgiacomo@gmail.com>
2020-12-12 14:58:36 +0100

    Refactoring model e dao decesso, non terminato

--- controllers/InsertDecessiController.java
+++ controllers/InsertDecessiController.java
@@ -2,10 +2,16 @@ package elaborato_ingegneriaSW.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import elaborato_ingegneriaSW.dao.DecessoDaoImpl;
 import elaborato_ingegneriaSW.dao.ProvinciaDaoImpl;
+import elaborato_ingegneriaSW.dao.RegioneDaoImpl;
+import elaborato_ingegneriaSW.models.Decesso;
 import elaborato_ingegneriaSW.models.Provincia;
+import elaborato_ingegneriaSW.models.Regione;
 import elaborato_ingegneriaSW.models.Territorio;
 import elaborato_ingegneriaSW.utils.AutoCompleteBox;
+import elaborato_ingegneriaSW.utils.FXUtil;
 import elaborato_ingegneriaSW.utils.ShowView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,31 +20,67 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 
-public class InsertDecessiController  implements Initializable {
+public class InsertDecessiController implements Initializable {
     @FXML
-    private JFXComboBox provinciaComboBox;
+    private JFXComboBox provinciaFilterComboBox;
     @FXML
-    private JFXComboBox annoComboBox;
+    private JFXComboBox annoFilterComboBox;
     @FXML
-    private JFXButton ricercaButton;
+    private JFXButton ricercaFilterButton;
     @FXML
     private JFXButton insertDecessiButton;
+    @FXML
+    private JFXTextField incidenteStradaleNumeroTextField;
+    @FXML
+    private JFXTextField malattieTumoraliNumeroTextField;
+    @FXML
+    private JFXTextField malattieCardiovascolariNumeroTextField;
+    @FXML
+    private JFXTextField malattieContagioseNumeroTextField;
+
+    private final ProvinciaDaoImpl provinciaDao = new ProvinciaDaoImpl();
+    private final DecessoDaoImpl decessoDao = new DecessoDaoImpl();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        try {
+            Set<Provincia> province = provinciaDao.getAllItems(ProvinciaDaoImpl.getCollectionName());
+
+            for (Provincia provincia: province) {
+                provinciaFilterComboBox.getItems().add(provincia);
+            }
+            new AutoCompleteBox(provinciaFilterComboBox);
+
+            Set<Decesso> decessi = decessoDao.getAllItems(DecessoDaoImpl.getCollectionName());
+            for (Decesso decesso: decessi) {
+                System.out.println(decesso.getNumeroMorti());
+                //annoFilterComboBox.getItems().add(decesso.getYear());
+            }
+            new AutoCompleteBox(annoFilterComboBox);
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void showInsertDecessi(ActionEvent event) throws IOException {
--- models/Decesso.java
+++ models/Decesso.java
@@ -1,10 +1,11 @@
 package elaborato_ingegneriaSW.models;
 
-public class Decesso {
+import java.util.Objects;
+
+public class Decesso implements Comparable<Decesso>{
     private Provincia provincia;
     private int year;
     private int numeroMorti;
-
     private CausaDecesso causaDecesso;
 
     /**
@@ -53,5 +54,39 @@ public class Decesso {
 
     public void setCausaDecesso(CausaDecesso causaDecesso) {
         this.causaDecesso = causaDecesso;
+    }
+
+    /**
+     * Ritorna l'id univoco per il record nel database
+     * @return year + provincia name
+     */
+    public String generateId() {
+        return year + "_" + provincia.getNome().toLowerCase();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Decesso decesso = (Decesso) o;
+        return Objects.equals(decesso.getYear(), decesso.getProvincia().getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(provincia.getNome() + year);
+    }
+
+    @Override
+    public String toString() {
+        return provincia.getNome() + " " + year;
+    }
+
+    @Override
+    public int compareTo(Decesso other) {
+        if(year == other.getYear()){
+            return provincia.getNome().compareTo(other.getProvincia().getNome());
+        }
+        return 0;
     }
 }
